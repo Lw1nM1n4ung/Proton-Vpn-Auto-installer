4cde3a8dd049e65a7a376e4d38c141b25a59ed20
Lwin Min Aung <am8680623@gmail.com>
2023-12-09 04:55:46 +0630

    main

---
+++ main.sh
@@ -0,0 +1,30 @@
+echo -e "Proton vpn installer."
+echo -e "script by Demon"
+echo -e ""
+echo -e ""
+echo -e "[1] > Debain"
+echo -e "[2] > Ubuntu"
+echo -e "[3] > Redhat and Centos"
+echo ""
+a="1"
+while [ "1" -eq "$a" ]
+do
+read -p ">>>>" choose
+case $choose in
+
+  1)
+    bash Debian/Debain.sh
+    a=2
+    ;;
+
+  2)
+    bash Ubuntu/Ubuntu.sh
+    a=2
+    ;;
+
+  3)
+    bash Red_Hat_Enterprise_Linux_and_CentOS/Red_Hat_Enterprise_Linux_and_CentOS.sh
+    a=2
+    ;;
+esac
+done
